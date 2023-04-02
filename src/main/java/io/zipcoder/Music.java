001943d5544850b06d593b229017b79a4c07df09
timothy <pimtarka@gmail.com>
2023-04-02 16:02:41 -0400

    finished playlist

--- src/main/java/io/zipcoder/Music.java
+++ src/main/java/io/zipcoder/Music.java
@@ -1,14 +1,44 @@
 package io.zipcoder;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Music {
+    String[] playlist;
 
-    private String[] playList;
-
-    public Music(String[] playList){
-        this.playList = playList;
+    public Music(String[] playlist) {
+        this.playlist = playlist;
     }
 
-    public Integer selection(Integer startIndex, String selection){
-        return null;
+    public Integer selection(int startingIndex, String selection) {
+        int index = -1;
+        int up = 0, down = 0;
+
+        // Find all indices of the target song q
+        List<Integer> song = new ArrayList<Integer>();
+        for (int i = 0; i < playlist.length; i++) {
+            if (playlist[i].equals(selection)) {
+                song.add(i);
+            }
+        }
+
+        // Calculate the number of button presses needed to reach q
+        int minPresses = Integer.MAX_VALUE;
+        for (int indexTemp : song) {
+            if (indexTemp == startingIndex) {
+                return 0;
+            }
+            if (indexTemp < startingIndex) {
+                up = startingIndex - indexTemp;
+                down = playlist.length - up;
+            } else {
+                down = indexTemp - startingIndex;
+                up = playlist.length - down;
+            }
+            minPresses = Math.min(minPresses, Math.min(up, down));
+        }
+
+        // Return the minimum number of button presses
+        return minPresses;
     }
 }
